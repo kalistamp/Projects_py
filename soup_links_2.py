1531ae783a35d6ed498b82a0051081ea6c600fc3
kalistamp <81341243+kalistamp@users.noreply.github.com>
2022-01-27 20:08:37 -0800

    Update soup_links_2.py

--- soup_links_2.py
+++ soup_links_2.py
@@ -17,7 +17,9 @@ print(x)
 print(Fore.MAGENTA + spam.title())
 print(x*2)
 
-URL = input(f'Enter Link to be parsed for all embedded Links:    ')
+
+
+URL = input(f'Enter Link to be parsed for all embedded Links:   ')
 
 # Add an if/else statement to include "https" befpre the url if it is not already included
 
@@ -28,6 +30,7 @@ else:
     page_data = requests.get('https://' + URL)
     print(page_data)
 
+print(x*2)
 # By adding "print(page_data)" the output will show a "Response [200]" if the request was successful 
 
 soup_parser = BeautifulSoup(page_data.text, 'html.parser')
@@ -39,8 +42,10 @@ links_grabbed = []
 for link in soup_parser.find_all('a'):
     links_grabbed.append(link.get('href') + '\n')
 
-with open('kaliscrape.txt', 'a') as done:
+with open('kaliscrape_NG.txt', 'a') as done:
     print(*links_grabbed, sep = '\n', file = done )
+    print('[+] Total Number of links Found: ', len(URL))
+
 
 # When finally printing and saving "done" file, add: sep='\n' to print out everything extracted in clean format
 
