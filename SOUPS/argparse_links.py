e01db87e758092c95023ff05a6f0c53a11ebdb61
kalistamp <81341243+kalistamp@users.noreply.github.com>
2022-02-17 19:20:49 -0800

    Update argparse_links.py

--- SOUPS/argparse_links.py
+++ SOUPS/argparse_links.py
@@ -92,4 +92,4 @@ print(x*2)
 # 2. Take the URL as an input
 # 3. Validate the URL for 'http' and 'https'
 # 4. Define an empty array - - ? (WTF Does that mean ..)
-# 5. Append the data in that array and save the data in the kaliscrape_NG.txt file
+# 5. Append the data in that array and save the data in the {url}_links.txt file
