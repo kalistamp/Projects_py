161aea912ee95c5c3e6e947571df53631d035d26
kalistamp <81341243+kalistamp@users.noreply.github.com>
2022-04-21 18:39:18 -0700

    Update news.py

--- SOUPS/news.py
+++ SOUPS/news.py
@@ -16,8 +16,11 @@ MAG = Fore.MAGENTA
 RESET = colorama.Fore.RESET
 LIT = Style.BRIGHT
 
+
 print(f'{RED}{LIT}Table of Contents:')
-print(f'{RED}cstop:{RESET} The Basics ')
+print(f'{RED}{LIT}cstop:{RESET} The Basics ')
+print(f'{RED}{LIT}tech:{RESET} Tech related News ')
+print(f'{RED}{LIT}Xtras(x):{RESET} All the Extras ')
 
 def Cstop_news():
     choice = input(f'What Section of news would you like to read today? \n [Enter Acronym]:     ')
@@ -26,25 +29,30 @@ def Cstop_news():
         webbrowser.open_new_tab('https://news.ycombinator.com/')
         webbrowser.open_new_tab('https://www.hackingarticles.in')
         webbrowser.open_new_tab('https://www.ibtimes.com')
+        webbrowser.open_new_tab('https://www.theepochtimes.com/')
+        webbrowser.open_new_tab('https://www.mintpressnews.com/')
+        webbrowser.open_new_tab('https://therecord.media/')
         webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-    elif choice == 'Tech':
+    elif choice == 'tech':
         webbrowser.open_new_tab('https://krebsonsecurity.com/')
         webbrowser.open_new_tab('https://dumpoir.com/v/llllap3xllll')  # He Posts Awesome Repos :)
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
+        webbrowser.open_new_tab('https://securityaffairs.co/wordpress/')
+        webbrowser.open_new_tab('https://cybersecurity-magazine.com/')
+        webbrowser.open_new_tab('https://www.hackerone.com/vulnerability-and-security-testing-blog')
+        webbrowser.open_new_tab('https://telegra.ph/Razvedka-dlya-podgotovki-DDoS-atak-03-05')
+        webbrowser.open_new_tab('https://telegra.ph/KORISN%D0%86-POSILANNYA-DLYA-DOPOMOGI-U-DDOS-ATAC%D0%86-02-27')
+        webbrowser.open_new_tab('https://www.secjuice.com/tag/osint/')
+        webbrowser.open_new_tab('https://www.wonderhowto.com/')
         webbrowser.open_new_tab('https://hakin9.org/blog-2/')
         webbrowser.open_new_tab('https://www.hackingarticles.in')
-        webbrowser.open_new_tab('https://www.ibtimes.com')
-    elif choice == 'Xtras':
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
-        webbrowser.open_new_tab(' ')
+    elif choice == 'x':
+        webbrowser.open_new_tab('http://unlimitedhangout.com/')
+        webbrowser.open_new_tab('https://www.thelastamericanvagabond.com/')
+        webbrowser.open_new_tab('https://www.investmentwatchblog.com/author/maizipeng/')
+        webbrowser.open_new_tab('https://thegradient.pub/')
+        webbrowser.open_new_tab('https://www.sciencedaily.com/')
+        webbrowser.open_new_tab('https://www.propublica.org/')
+        webbrowser.open_new_tab('https://viewdns.info/iphistory/?domain=therecap.org')
     else:
         print('It seems you made a typo, that secrtion does not exist')
 
