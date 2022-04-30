48dfbb586a1f7b45249e7e70c09981817ac1643a
kalistamp <81341243+kalistamp@users.noreply.github.com>
2022-04-30 10:45:43 -0700

    Create v1rus_check.py

---
+++ OPEN/v1rus_check.py
@@ -0,0 +1,32 @@
+# Requirements
+# By: калиштамп
+
+import os
+
+sources = [       'https://github.com/kalistamp',
+'https://www.virustotal.com/gui/home/upload',
+'https://vms.drweb.com/online/?lng=en',
+'https://virusshare.com/',
+'https://www.eset.com/us/home/online-scanner/',
+'https://t.me/VirusTotalAV_bot',
+'https://telegram.me/drwebbot'
+
+ ]
+
+# print(*sources, sep = '\n')
+for number, links in enumerate(sources):
+    print(number, links)
+
+file = open("Anti_germs", "w") 
+file.write("""
+
+https://github.com/kalistamp
+https://www.virustotal.com/gui/home/upload
+https://vms.drweb.com/online/?lng=en
+https://virusshare.com/
+https://www.eset.com/us/home/online-scanner/
+https://t.me/VirusTotalAV_bot
+https://telegram.me/drwebbot
+
+""")
+file.close()
