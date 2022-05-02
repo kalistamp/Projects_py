a23a60c367ca7d735189e752d6fe56f248398315
kalistamp <81341243+kalistamp@users.noreply.github.com>
2022-05-01 22:37:04 -0700

    Update v1rus_check.py

--- OPEN/v1rus_check.py
+++ OPEN/v1rus_check.py
@@ -13,12 +13,16 @@ sources = [       'https://github.com/kalistamp',
 
  ]
 
-# print(*sources, sep = '\n')
-for number, links in enumerate(sources):
-    print(number, links)
-
-file = open("Anti_germs", "w") 
-file.write("""
+def v_list():
+    choice = input(f'Type \"show\", or \"print\" to get Virus Check Resources \n Which would you like: ')
+    if choice == 'show':
+        for number, links in enumerate(sources):
+            print(number, links)
+    elif choice == 'print':
+        for number, links in enumerate(sources):
+            print(number, links)
+        file = open("Anti_germs", "w") 
+        file.write("""
 
 https://github.com/kalistamp
 https://www.virustotal.com/gui/home/upload
@@ -28,7 +32,10 @@ https://www.eset.com/us/home/online-scanner/
 https://t.me/VirusTotalAV_bot
 https://telegram.me/drwebbot
 
-""")
-file.close()
+        """)
+        file.close()
+        print('[+] Recource list printed in current directory')
+    else:
+        print(' [!] It seems you made a typo, that option does not exist')
 
-print(' [+] Recource list printed in current directory')
+v_list()
